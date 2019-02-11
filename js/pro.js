e1664846450b31e2cceda3fc50938ba40bcbfb2b
nikhilakn <36179799+nikhilakn@users.noreply.github.com>
2019-02-11 14:16:05 +0530

    Hyper Link added for each projects

--- js/pro.js
+++ js/pro.js
@@ -21,7 +21,7 @@ if(name != "")
 	{
 		if(projects[i].owners == name)
 	 		{
-				display_string+="!<li>"+projects[i].title+"</li>";
+				display_string+="!<li><a href='"+projects[i].pagelink+"'>"+projects[i].title+"</a></li>";
 			}
 	}
 	return display_string+"</ol>";
@@ -30,7 +30,7 @@ else
 {
 	for(var i = 0; i<projects.length; i++)
 	{
-		display_string+="!<li>"+projects[i].title+"</li>";
+		display_string+="!<li><a href='"+projects[i].pagelink+"'>"+projects[i].title+"</a></li>";
 	}
 	return display_string;
 }
@@ -42,42 +42,55 @@ var allProject = {
     "project" : [
       {
         "title" : "Software Engineering for Social Impact",
-        "owners" :"Sujit Kumar Chakrabarti"
+        "owners" :"Sujit Kumar Chakrabarti",
+	"pagelink" : "../projects/social-sujit.html"
       },{
         "title" : "Automated Evaluation of Programming Assignments",
-        "owners" : "Sujit Kumar Chakrabarti"
+        "owners" : "Sujit Kumar Chakrabarti",
+	"pagelink" : "../projects/autoeval-sujit.html"
       },{
         "title" : "Detection of Architectural Knowledge from Legacy Code",
-        "owners" : "Sujit Kumar Chakrabarti"
+        "owners" : "Sujit Kumar Chakrabarti",
+	"pagelink" : "../projects/reveng-sujit.html"
       },{
         "title" : "Symbolic Testing of Embedded Software",
-        "owners" : "Sujit Kumar Chakrabarti"
+        "owners" : "Sujit Kumar Chakrabarti",
+	"pagelink" : "../projects/symtest-sujit.html"
       },{
         "title" : "Formal Specification of Web apps using Statecharts",
-        "owners" : "Sujit Kumar Chakrabarti"
+        "owners" : "Sujit Kumar Chakrabarti",
+	"pagelink" : "../projects/reqeng-sujit.html"
       },{
         "title" : "Verification and validation of architectures for embedded software",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-1.html"
       },{
         "title" : "Verification and validation of IoT systems",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-2.html"
       },{
         "title" : "Verifiable avionics self-adaptive software",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-3.html"
       },{
         "title" : "Program analysis, test case generation and motion planning for robotics software",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-4.html"
       },{
         "title" : "Verifying data races and timing requirements in RTOS",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-5.html"
       },{
         "title" : "Architecture and design decisions in complex systems and system-of-systems",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-6.html"
       },{
         "title" : "Formal verification of Simulink models",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-7.html"
       },{ "title" : "Other projects",
-        "owners" : "Meenakshi D'Souza"
+        "owners" : "Meenakshi D'Souza",
+	"pagelink" : "../projects/project-md-8.html"
       }
     ]
   };

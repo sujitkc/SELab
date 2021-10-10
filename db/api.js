60e3b8abc2157646c93525e1d319aab9d2563c6c
sujitkc <sujitkc@gmail.com>
2021-10-10 17:33:10 +0530

    Added code to people/people.html to display list of faculty members and research students. This marks the first use of data model to display data on webpages. api.js is the interface for utility functions for HTML rendering of commonly displayed data commonly used across webpages, e.g. list of  people or list of publications.

--- db/api.js
+++ db/api.js
@@ -1,5 +1,15 @@
 // HTML dump of publication array
 // Where to use: Wherever a list of publications needs
+function reduceStrings(strings) {
+  var s = "<ul>";
+  s+= strings.reduce(
+    function(x, y) { return x + "<li>" + y; },
+    ""
+  );
+  s += "</ul>";
+  return s;
+}
+
 // to be added to an HTML file, typically in the 
 // person webpages of all the lab members.
 function html_of_publications(publications) {
@@ -14,3 +24,12 @@ function html_of_publications(publications) {
     );
   return strPubs;
 }
+
+function peopleListtoHTML(list) {
+  let theLab = Lab.getInstance();
+  let htmls = list.map(
+    function(x) { return x.toHTML(); }
+  );
+  return reduceStrings(htmls);
+}
+
--- db/lab.js
+++ db/lab.js
@@ -36,10 +36,11 @@ const registration = {
 }
 
 class Person {
-  constructor(id, name, type) {
+  constructor(id, name, type, webpage) {
     this.id = id;
     this.name = name;
     this.type = type;
+    this.webpage = webpage;
   }
 
   getPublications() {
@@ -50,24 +51,30 @@ class Person {
       }
     );
   }
+
+  toHTML() {
+    return "<a href=\"" + this.webpage + "\">" + this.name.fullName() + "</a>";
+  }
 }
 
 class Faculty extends Person {
-  constructor(id, name, empnum) {
-    super(id, name, membertype.MEMBERFACULTY);
+  constructor(id, name, empnum, webpage) {
+    super(id, name, membertype.MEMBERFACULTY, webpage);
     this.employeeNumber = empnum;
   }
 
   getStudents() {
-    faculty = this;
+    let fac = this;
     let theLab = Lab.getInstance();
+    console.log("faculty = " + fac.id);
     return theLab.getMemberStudents().filter(
       function(x) {
-        return (x.supervisor == faculty) || (x.coSupervisors.includes(faculty));
+        return (x.supervisor == fac) || (x.coSupervisors.includes(fac));
       }
     );
   }
 
+  // temporary -- just for testing; to be removed.
   getPublications() {
     return [this.id, this.name, "publication"];
   }
@@ -84,8 +91,10 @@ class Student extends Person {
       joiningYear,
       graduatingYear,
       programme,
-      registrationType) {
-    super(id, name, membertype.MEMBERSTUDENT);
+      registrationType,
+      webpage
+  ) {
+    super(id, name, membertype.MEMBERSTUDENT, webpage);
     this.rollNumber       = rollNumber;
     this.supervisor       = supervisor;
     this.coSupervisors    = coSupervisors;
@@ -104,9 +113,12 @@ class Student extends Person {
 class Lab {
 
   static instance = null;
+
+  static baseURL = "file:///home/sujit/IIITB/SELab/website/SELab/";
+
   constructor() {
-    this.people = [];
-    this.conferences = [];
+    this.people       = [];
+    this.conferences  = [];
     this.publications = [];
 
     this.addFacultyMembers();
@@ -121,36 +133,49 @@ class Lab {
     this.addTechReports();
   }
 
+  // extra Person specific fields from the json object in people.
+  extractPersonDetails(p) {
+    let firstName  = p["First Name"];
+    let middleName = p["Middle Name"];
+    let lastName   = p["Last Name"];
+    let name = new Name(firstName, middleName, lastName);
+    var webpage = null;
+    if(p["webpage"] != null) {
+      if(p["linktype" == "absolute"]) {
+        webpage = p["webpage"];
+      }
+      else {
+        webpage = Lab.baseURL + p["webpage"];
+      }
+    }
+    else {
+      webpage = "";
+    }
+    return new Person(null, name, null, webpage);
+  }
+
   addFacultyMembers() {
     for(let fac of faculty) {
-      var name = null;
+      var person = null;
       for(let p of people) {
         if(p.Email == fac.Email) {
-          let firstName = p["First Name"];
-          let middleName = p["Middle Name"];
-          let lastName = p["Last Name"];
-          name = new Name(firstName, middleName, lastName);
+          person = this.extractPersonDetails(p);
           break;
         }
       }
-     
-      var f = new Faculty(fac.Email, name, fac.employeeID); 
+      var f = new Faculty(fac.Email, person.name, fac.employeeID, person.webpage); 
       this.people.push(f);
     }
   }
 
   addStudents() {
-    faculty = this.getMemberFaculty();
+    let faculty = this.getMemberFaculty();
+    var person = null; 
     for(let st of students) {
       var name = null;
       for(let p of people) {
         if(p.Email == st.Email) {
-          let firstName = p["First Name"];
-          let middleName = p["Middle Name"];
-          let lastName = p["Last Name"];
-          name = new Name(firstName, middleName, lastName);
-
-          break;
+          person = this.extractPersonDetails(p);
         }
       }
 
@@ -163,7 +188,7 @@ class Lab {
         jterm = term.AUGUST;
       }
       var prog = null;
-      if(st["Programme"] == "Ph.D") {
+      if(st["Programme"] == "Ph.D.") {
         prog = programme.PHD;
       }
       else if(st["Programme"] == "MS") {
@@ -178,6 +203,9 @@ class Lab {
       else if(st["Programme"] == "M.Sc Digital Society") {
         prog = programme.MSCDT;
       }
+      else {
+        prog = programme.PHD;
+      }
       var registrationType = null;
       if(st["Registration Type"] == "Full Time") {
         registrationType = registration.FT;
@@ -188,7 +216,7 @@ class Lab {
       let gyear = parseInt(st["Graduating Year"]);
       var student = new Student(
                       st.Email,
-                      name,
+                      person.name,
                       st["Roll Number"],
                       null,
                       [],
@@ -196,7 +224,8 @@ class Lab {
                       jyear,
                       gyear,
                       prog,
-                      registrationType);
+                      registrationType,
+                      person.webpage);
       let sups = supervisor.filter(
         function(x) { 
           return x["Student"] == student.id && x["Role"] == "Supervisor";
